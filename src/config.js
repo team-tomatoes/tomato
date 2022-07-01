33f5081ab4026af92cf239270505f3ecdfcbe0b5
Cindy Yueh <cindyyueh@Cindys-MacBook-Pro.local>
2022-07-01 15:08:59 -0400

    changed firebase stuff

--- src/config.js
+++ src/config.js
@@ -1,15 +1,38 @@
-const defaultAvatar = 'https://firebasestorage.googleapis.com/v0/b/expo-boilerplate-v2.appspot.com/o/icon.png?alt=media&token=505f30e6-6702-410a-84e7-f66b3ff60daf'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseKey = {
-  apiKey: "AIzaSyAbynvh81TkAmQE2lBY8JEDOVpNaHZOozU",
-  authDomain: "expo-boilerplate-v2.firebaseapp.com",
-  projectId: "expo-boilerplate-v2",
-  storageBucket: "expo-boilerplate-v2.appspot.com",
-  messagingSenderId: "852442919227",
-  appId: "1:852442919227:web:313aba2cd455b34701871e",
-  measurementId: "G-1894ZG59D5"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyC9SB_2B_Ffzdw1y_qn17Vdk15puWszjBk',
+  authDomain: 'tomato-d9ece.firebaseapp.com',
+  projectId: 'tomato-d9ece',
+  storageBucket: 'tomato-d9ece.appspot.com',
+  messagingSenderId: '764902374604',
+  appId: '1:764902374604:web:d6ead29ee682cb54301669',
+  measurementId: 'G-3N394GK8Y1',
 }
 
-const eulaLink = 'https://github.com/kiyohken2000/ReactNative-Expo-Firebase-Boilerplate-v2'
+const defaultAvatar = 'gs://tomato-d9ece.appspot.com/tomatoicon.png'
 
-export { defaultAvatar, firebaseKey, eulaLink }
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
+// const firebaseKey = {
+//   apiKey: "AIzaSyAbynvh81TkAmQE2lBY8JEDOVpNaHZOozU",
+//   authDomain: "expo-boilerplate-v2.firebaseapp.com",
+//   projectId: "expo-boilerplate-v2",
+//   storageBucket: "expo-boilerplate-v2.appspot.com",
+//   messagingSenderId: "852442919227",
+//   appId: "1:852442919227:web:313aba2cd455b34701871e",
+//   measurementId: "G-1894ZG59D5"
+// }
+
+const eulaLink =
+  'https://github.com/kiyohken2000/ReactNative-Expo-Firebase-Boilerplate-v2'
+
+export { defaultAvatar, firebaseConfig, eulaLink }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,30 +1,30 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app'
 // authentication
-import { initializeAuth } from 'firebase/auth';
-import { getReactNativePersistence } from 'firebase/auth/react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth } from 'firebase/auth'
+import { getReactNativePersistence } from 'firebase/auth/react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 // firestore
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore'
 // cloud storage
-import { getStorage } from "firebase/storage";
+import { getStorage } from 'firebase/storage'
 
-import { firebaseKey } from '../config'
+import { firebaseConfig } from '../config'
 
-const firebaseConfig = {
-  apiKey: firebaseKey.apiKey,
-  authDomain: firebaseKey.authDomain,
-  projectId: firebaseKey.projectId,
-  storageBucket: firebaseKey.storageBucket,
-  messagingSenderId: firebaseKey.messagingSenderId,
-  appId: firebaseKey.appId,
-  measurementId: firebaseKey.measurementId
-};
+const firebaseKey = {
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+  appId: firebaseConfig.appId,
+  measurementId: firebaseConfig.measurementId,
+}
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseKey)
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
-});
-const firestore = getFirestore();
-const storage = getStorage();
+})
+const firestore = getFirestore()
+const storage = getStorage()
 
-export { auth, firestore, storage };
+export { auth, firestore, storage }
