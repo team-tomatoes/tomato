d1e33b7a90e147dce72c8e88426f1ec5288e888b
rachelj521 <rjiang521@gmail.com>
2022-07-07 18:05:43 -0400

    test friends rendering on list

--- src/scenes/friendsList/FriendsList.js
+++ src/scenes/friendsList/FriendsList.js
@@ -22,42 +22,58 @@ export default function Friends() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    async function fetchFriends() {
-      try {
-        // const querySnapshot = await getDocs(collection(firestore, 'users'))
-        // querySnapshot.forEach((doc) => {
-        //   console.log(doc.id, ' => ', doc.data())
-        // })
-        // console.log('USER DATA FRIENDS', userData.friends)
-
-        const usersRef = collection(firestore, 'users')
-        const q = query(usersRef, where('id', '==', `${userId}`))
-
-        const userDetails = []
-
-        const querySnapshot = await getDocs(q)
-
-        querySnapshot.forEach((doc) => {
-          userDetails.push(doc.data())
-          console.log('USER DETAILS', userDetails)
-          // console.log(doc.id, ' => ', doc.data())
-        })
-        // console.log('QUERY SNAPSHOT', querySnapshot)
-        setFriends(userDetails.friends)
-        setLoading(false)
-      } catch (error) {
-        console.log('error fetching user friends!', error)
-      }
-    }
-    fetchFriends()
+    setFriends(userData.testfriend)
   }, [])
+
+  console.log(friends)
+
+  // useEffect(() => {
+  //   async function fetchFriends() {
+  //     try {
+  // const querySnapshot = await getDocs(collection(firestore, 'users'))
+  // querySnapshot.forEach((doc) => {
+  //   console.log(doc.id, ' => ', doc.data())
+  // })
+  // console.log('USER DATA FRIENDS', userData.friends)
+  // const userFriends = userData.testfriend
+
+  // const usersRef = collection(firestore, 'users')
+  // const q = query(usersRef, where('id', '==', `${userId}`))
+
+  // // const userDetails = []
+
+  // const querySnapshot = await getDocs(q)
+  // let friendData
+  // querySnapshot.forEach((doc) => {
+  //   // console.log(doc.id, ' => ', doc.get('friends'))
+  //   friendData = doc.get('friends')
+  // })
+  // setFriends(friendData)
+  // console.log('friendData!!!', friendData)
+  // friendData.forEach((friend) => {
+  //   const friendsRef = collection(firestore, 'users')
+  // })
+  // console.log('friends!!!', friends)
+  // console.log('QUERY SNAPSHOT', querySnapshot)
+  // setFriends(userDetails.friends)
+  //       setLoading(false)
+  //     } catch (error) {
+  //       console.log('error fetching user friends!', error)
+  //     }
+  //   }
+  //   fetchFriends()
+  // }, [])
 
   return (
     <ScreenTemplate>
       <View style={[styles.container]}>
         <View style={{ width: '100%' }}>
           <Text style={[styles.field, { color: colorScheme.text }]}>
-            Friends List goes here
+            {friends.map((friend) => (
+              <Text key={friend.id}>
+                {friend.name}
+              </Text>
+            ))}
           </Text>
           <Button
             label="Open Modal"
