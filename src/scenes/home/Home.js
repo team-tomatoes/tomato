2c9202dbe14c4397b6119387b6c6fc2d4b727c40
Cindy Yueh <cindyyueh@Cindys-MacBook-Pro.local>
2022-07-07 16:15:42 -0400

    got date into the db

--- src/scenes/home/Home.js
+++ src/scenes/home/Home.js
@@ -28,7 +28,6 @@ import {
   getFirestore,
   collection,
 } from 'firebase/firestore'
-import firebase from 'firebase/app'
 import { colors, fontSize } from 'theme'
 import { Video, AVPlaybackStatus } from 'expo-av'
 import Button from '../../components/Button'
@@ -133,7 +132,7 @@ export default function Home() {
                 await addDoc(collection(firestore, 'pins'), {
                   category: 'Animal-Sightings',
                   coordinates: [Number(currLatitude), Number(currLongitude)],
-                  date: firebase.firestore.Timestamp.now(),
+                  date: new Date(),
                   description,
                   photo: '',
                   subcategory: 'Rat',
