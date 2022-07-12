c6a0d1bab54c2f6cd80fcadce452317fec9318cf
Cindy Yueh <cindyyueh@Cindys-MBP.fios-router.home>
2022-07-12 15:06:03 -0400

    got correct current address to load

--- src/components/ExploreMap.js
+++ src/components/ExploreMap.js
@@ -29,7 +29,6 @@ export const PinnedMap = () => {
   const [pins, setPins] = useState([])
   const [users, setUsers] = useState([])
   const [modalData, setModalData] = useState([])
-  const [latLong, setLatLong] = useState([])
   const [near, setNear] = useState('')
   const [modalVisible, setModalVisible] = useState(false)
 
@@ -64,31 +63,37 @@ export const PinnedMap = () => {
       querySnapshot.forEach((user) => {
         userArr.push([user.data().id, user.data().userName])
       })
+      console.log('USERARR', userArr)
       setUsers(userArr)
     } catch (err) {
       console.log(err)
     }
   }
 
+  // const findUserName = (userId) => {
+  //   for (let i = 0; i < users.length; i + 1) {
+  //     if (userId === users[0]) {
+  //       return (users[1])
+  //     }
+  //     console.log('no user found!')
+  //   }
+  // }
+
   useEffect(() => {
     loadAllPins()
     loadUsers()
   }, [])
 
-  const loadNear = async () => {
+  const loadNear = async (latLong) => {
     try {
       await Geocoder.from(latLong).then((json) => {
         const addressComponent = json.results[0].formatted_address
-        console.log(addressComponent)
         setNear(addressComponent)
-        console.log(near)
       })
     } catch (err) {
       console.log(err)
     }
   }
-
-  console.log(near)
 
   return (
     <>
@@ -131,13 +136,11 @@ export const PinnedMap = () => {
                 latitude: pin[0],
                 longitude: pin[1],
               }}
-              // description={pin[3]}
               image={icon()}
               onPress={() => {
                 setModalData(pin)
+                loadNear([pin[0], pin[1]])
                 setModalVisible(true)
-                setLatLong([pin[0], pin[1]])
-                loadNear(latLong)
               }}
             />
           )
@@ -156,9 +159,12 @@ export const PinnedMap = () => {
           <View style={styles.modalView}>
             <Text style={styles.modalDescriptionText}>{modalData[2]}</Text>
             <Text style={styles.modalNearText}>Near {near}</Text>
-            <Image style={{ height: 250, width: 150 }} source={{ uri: modalData[4] }} />
+            <Image
+              style={{ height: 250, width: 150 }}
+              source={{ uri: modalData[4] }}
+            />
             <Text style={styles.modalText}>{modalData[3]}</Text>
-            <Text style={styles.modalText}>{modalData[3]}</Text>
+            <Text style={styles.modalText}>{modalData[5]}</Text>
             <Pressable
               style={[styles.button, styles.buttonClose]}
               onPress={() => setModalVisible(!modalVisible)}
