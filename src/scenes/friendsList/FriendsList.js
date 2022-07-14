63b0d3cd46afe18d9b69601fe2716c82ed46f06e
Cindy Yueh <cindyyueh@Cindys-MacBook-Pro.local>
2022-07-13 20:40:51 -0400

    added modal that pops up for specific user on friend list

--- src/scenes/friendsList/FriendsList.js
+++ src/scenes/friendsList/FriendsList.js
@@ -1,8 +1,29 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { Text, View, StyleSheet, SafeAreaView, FlatList } from 'react-native'
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Alert,
+  Modal,
+  Pressable,
+} from 'react-native'
+import { Avatar } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
 import { colors, fontSize } from 'theme'
-import { getDocs, collection, query, where, doc, updateDoc, onSnapshot, arrayUnion, arrayRemove } from 'firebase/firestore'
+import {
+  getDocs,
+  collection,
+  query,
+  where,
+  doc,
+  updateDoc,
+  onSnapshot,
+  arrayUnion,
+  arrayRemove,
+  getDoc,
+} from 'firebase/firestore'
 import { firestore } from '../../firebase/config'
 import { ColorSchemeContext } from '../../context/ColorSchemeContext'
 import { UserDataContext } from '../../context/UserDataContext'
@@ -19,6 +40,9 @@ export default function Friends() {
   }
   const uid = userData.id
   const [friends, setFriends] = useState([])
+  const [modalVisible, setModalVisible] = useState(false)
+  const [friendModalData, setFriendModalData] = useState([])
+  const [pinNumber, setPinNumber] = useState(null)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -30,7 +54,7 @@ export default function Friends() {
         let friendData = []
         onSnapshot(q, (querySnapshot) => {
           querySnapshot.forEach((document) => {
-            friendData = (document.data().friendsList)
+            friendData = document.data().friendsList
           })
           setFriends(friendData)
         })
@@ -81,19 +105,97 @@ export default function Friends() {
           data={friends}
           renderItem={({ item }) => (
             <>
-              <Text style={[styles.item, { color: colorScheme.text }]}>{item.userName}</Text>
-              <Button label="View" color={colors.primary}>View</Button>
-              <Button label="Delete" color={colors.primary} onPress={() => onPressDeleteFriend(item)}>Delete</Button>
+              <Text style={[styles.item, { color: colorScheme.text }]}>
+                {item.userName}
+              </Text>
+              <Button
+                label="View"
+                color={colors.primary}
+                onPress={async () => {
+                  const pinsArr = []
+                  const q = query(
+                    collection(firestore, 'pins'),
+                    where('user', '==', item.id),
+                  )
+
+                  const querySnapshot = await getDocs(q)
+
+                  querySnapshot.forEach((document) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    pinsArr.push([document.id])
+                  })
+                  setPinNumber(pinsArr.length)
+                  console.log(pinsArr.length)
+
+                  const docRef = doc(firestore, 'users', `${item.id}`)
+                  const docSnap = await getDoc(docRef)
+                  if (docSnap.exists()) {
+                    console.log('Document data:', docSnap.data())
+                    setFriendModalData(docSnap.data())
+                    console.log(friendModalData)
+                  } else {
+                    console.log('No such document!')
+                  }
+                  setModalVisible(true)
+                }}
+              >
+                View
+              </Button>
+              <Button
+                label="Delete"
+                color={colors.primary}
+                onPress={() => onPressDeleteFriend(item)}
+              >
+                Delete
+              </Button>
             </>
           )}
           keyExtractor={(item) => item.id}
         />
+        <View style={styles.centeredView}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has been closed.')
+              setModalVisible(!modalVisible)
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.title}>{friendModalData.fullName}</Text>
+                <View style={styles.avatar}>
+                  <Avatar
+                    size="xlarge"
+                    rounded
+                    source={{ uri: friendModalData.avatar }}
+                  />
+                </View>
+                <Text style={styles.modalText}>Pin Count: {pinNumber}</Text>
+                <Text style={styles.modalText}>
+                  @{friendModalData.userName}
+                </Text>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </View>
       </SafeAreaView>
     </ScreenTemplate>
   )
 }
 
 const styles = StyleSheet.create({
+  avatar: {
+    margin: 30,
+    alignSelf: 'center',
+  },
   container: {
     flex: 1,
     padding: 50,
@@ -106,6 +208,51 @@ const styles = StyleSheet.create({
   },
   button: {
     fontSize: 30,
+    textAlign: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  // button: {
+  //   borderRadius: 20,
+  //   padding: 10,
+  //   elevation: 2,
+  // },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: fontSize.xxxLarge,
     textAlign: 'center',
   },
 })
